c3d1d512d86e168e38656677ec95893f55fdc1ea
Divya <dm.divya.mahajan@gmail.com>
2023-06-29 11:24:49 -0700

    login/reister button

--- components/Login.js
+++ components/Login.js
@@ -117,7 +117,7 @@ export default function Login() {
         onClick={submitHandler}
         className="w-full max-w-[40ch] border border-white border-solid uppercase py-2 duration-300 relative after:absolute after:top-0 after:right-full after:bg-white after:z-10 after:w-full after:h-full overflow-hidden hover:after:translate-x-full after:duration-300 hover:text-slate-900"
       >
-        <h2 className="relative z-20">SUBMIT</h2>
+        <h2 className="relative z-20">{isLoggingIn ? "Login" : "Register"}</h2>
       </button>
       <h2
         className="duration-300 hover:scale-110 cursor-pointer"
